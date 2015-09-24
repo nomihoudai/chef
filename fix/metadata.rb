59216640ce66afc8024dddb35b00e18f7203f1e2
jason <jason@getexp.com>
2015-09-24 13:56:17 -0700

    please

---
+++ fix/metadata.rb
@@ -0,0 +1,9 @@
+name        "opsworks_initial_setup_fix"
+description "Initial Setup for EC2 instances without destroying data"
+maintainer  "AWS OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends "deploy"
+
+recipe "opsworks_initial_setup::fixme", "please work"
